4f00735a09acb8e9c71e921cf2a08ebb98fd6ea2
huang <huangsheng8964@gmail.com>
2026-03-02 09:22:08 +0800

    fix: use inline SVG icon to bypass Twitter CSP restriction

--- tampermonkey/twitter-saver.user.js
+++ tampermonkey/twitter-saver.user.js
@@ -32,7 +32,6 @@
     });
 
     function getSaveIconHtml() {
-        const iconUrl = `${getBackendUrl()}/static/icon-128.png`;
         return `
         <div class="save-icon-circle" style="
             display: flex;
@@ -48,16 +47,19 @@
             box-sizing: border-box;
             transition: border-color 0.2s ease-out;
         ">
-            <img src="${iconUrl}"
-                 alt="Save"
-                 class="save-icon-img"
-                 style="
-                    width: 17px;
-                    height: 17px;
-                    display: block;
-                    pointer-events: none;
-                    opacity: 0.7;
-                 ">
+            <svg class="save-icon-img"
+                 xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24"
+                 fill="none"
+                 stroke="rgba(29,155,240,1)"
+                 stroke-width="2.5"
+                 stroke-linecap="round"
+                 stroke-linejoin="round"
+                 style="width:15px;height:15px;display:block;pointer-events:none;opacity:0.7;">
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+                <polyline points="7 10 12 15 17 10"/>
+                <line x1="12" y1="15" x2="12" y2="3"/>
+            </svg>
         </div>
     `;
     }
